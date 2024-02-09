d8d1960ec6c8ff5ee282d3ea7611cf8e4e9b4e5b
Rahul Dev Garg <34365102+rahuldevgarg@users.noreply.github.com>
2024-02-10 01:31:53 +0530

    fixes

--- frontend/mgramseva/lib/widgets/mobile_view.dart
+++ frontend/mgramseva/lib/widgets/mobile_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mgramseva/widgets/back.dart';
 import 'package:mgramseva/widgets/background_container.dart';
-import 'package:mgramseva/widgets/desktop_view.dart';
 import 'package:mgramseva/widgets/footer_banner.dart';
 
 class MobileView extends StatelessWidget {
@@ -30,61 +29,5 @@ class MobileView extends StatelessWidget {
             FooterBanner()
           ]),
     )));
-    return SingleChildScrollView(
-        child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-          Container(child: Center(child: LayoutBuilder(builder:
-              (BuildContext context, BoxConstraints viewportConstraints) {
-            return ConstrainedBox(
-                constraints: BoxConstraints(
-                    minHeight: MediaQuery.of(context).size.height-80),
-                child: IntrinsicHeight(
-                    child: Column(children: <Widget>[
-                  Expanded(
-                      // A flexible child that will grow to fit the viewport but
-                      // still be at least as big as necessary to fit its contents.
-                      child: Container(
-                          //height: 120.0,
-                          child: new Container(
-                              child: new Stack(
-                                  // // fit: StackFit.expand,
-                                  // clipBehavior: Clip.antiAlias,
-                                  children: <Widget>[
-                                (new Positioned(
-                                    bottom: 30.0,
-                                    child: new Container(
-                                        margin: EdgeInsets.only(bottom: 24),
-                                        width:
-                                            MediaQuery.of(context).size.width,
-                                        // height: MediaQuery.of(context).size.height + 20,
-                                        padding: EdgeInsets.all(8),
-                                        child: Column(
-                                          children: [
-                                            Row(
-                                              mainAxisAlignment: MainAxisAlignment.start,
-                                              children: [
-                                                Back(),
-                                              ],
-                                            ),
-                                            widget,
-                                          ],
-                                        )))),
-                                (new Positioned(
-                                    bottom: 0.0,
-                                    child: Container(
-                                      width: MediaQuery.of(context).size.width,
-                                      child: Row(
-                                        mainAxisAlignment: MainAxisAlignment.center,
-                                        children: [
-                                          FooterBanner(),
-                                        ],
-                                      ),
-                                    )))
-                              ]))))
-                ])));
-          })))
-        ]));
   }
 }
