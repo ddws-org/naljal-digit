5a78664bf44017e87cfff423fb9854cf747e1842
Rahul Dev Garg <34365102+rahuldevgarg@users.noreply.github.com>
2024-02-09 23:57:16 +0530

    fixes

--- frontend/mgramseva/lib/widgets/back.dart
+++ frontend/mgramseva/lib/widgets/back.dart
@@ -3,17 +3,14 @@ import 'package:flutter/material.dart';
 class Back extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Positioned(
-      top: 30.0,
-      child: IconButton(
-          icon: Icon(
-            Icons.arrow_back,
-          ),
-          iconSize: 25,
-          color: Color(0xfffa7a39),
-          splashColor: Colors.purple,
-          onPressed: () =>
-              Navigator.of(context, rootNavigator: true).maybePop()),
-    );
+    return IconButton(
+        icon: Icon(
+          Icons.arrow_back,
+        ),
+        iconSize: 25,
+        color: Color(0xfffa7a39),
+        splashColor: Colors.purple,
+        onPressed: () =>
+            Navigator.of(context, rootNavigator: true).maybePop());
   }
 }
