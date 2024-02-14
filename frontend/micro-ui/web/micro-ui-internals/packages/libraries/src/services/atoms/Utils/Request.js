e6207d31cc9a6295524f319b814d2078613e11ca
Anil Singha <99383116+anilsingha-eGov@users.noreply.github.com>
2024-02-14 12:48:57 +0530

    Updated request

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/atoms/Utils/Request.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/atoms/Utils/Request.js
@@ -68,7 +68,7 @@ export const Request = async ({
   multipartData = {},
   reqTimestamp = false,
 }) => {
-  url = `karnataka/${url}`;
+  url =  `karnataka/${url}`;
 
   const ts = new Date().getTime();
   if (method.toUpperCase() === "POST") {
