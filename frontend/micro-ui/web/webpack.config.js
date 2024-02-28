d823edefd4988f22c2e222d9a636fbdf2f414c1b
Anil Singha <99383116+anilsingha-eGov@users.noreply.github.com>
2024-02-28 18:10:19 +0530

    SM-3961: Added madhya pradesh setup

--- frontend/micro-ui/web/webpack.config.js
+++ frontend/micro-ui/web/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'build'),
-    publicPath: 'madhyapradesh/mgramseva-web/',
+    publicPath: '/madhyapradesh/mgramseva-web/',
   },
   optimization: {
     splitChunks: {
