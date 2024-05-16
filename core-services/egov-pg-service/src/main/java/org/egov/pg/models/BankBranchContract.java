3745f34776549cb3081464618e787124a72da23f
Saloni-eGov <saloni.bajaj@egovernments.org>
2024-05-16 11:35:00 +0530

    ISNE-101

--- core-services/egov-pg-service/src/main/java/org/egov/pg/models/BankBranchContract.java
+++ core-services/egov-pg-service/src/main/java/org/egov/pg/models/BankBranchContract.java
@@ -43,8 +43,8 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.*;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 @Builder
 @Getter
