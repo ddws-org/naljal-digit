0fba1983067c6aacb3b16349c2e1a91d133697f4
Saloni-eGov <saloni.bajaj@egovernments.org>
2024-05-16 17:14:46 +0530

    ISNE-101 dashboard-ingest

--- business-services/dashboard-ingest/src/main/java/com/ingestpipeline/IngestApp.java
+++ business-services/dashboard-ingest/src/main/java/com/ingestpipeline/IngestApp.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.ingestpipeline.util.Constants;
 
@@ -25,7 +24,7 @@ public class IngestApp {
 	
 	 @Bean
 	    public WebMvcConfigurer corsConfigurer() {
-	        return new WebMvcConfigurerAdapter() {
+	        return new WebMvcConfigurer() {
 	            @Override
 	            public void addCorsMappings(CorsRegistry registry) {
 	                registry.addMapping("/**").allowedMethods(Constants.ALLOWED_METHODS_GET,Constants.ALLOWED_METHODS_POST
