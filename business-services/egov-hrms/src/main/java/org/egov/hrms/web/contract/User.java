9999b688a379cd08c2c062b62a7c0998a9ed37fe
Taniya-eGov <taniyapushey@gmail.com>
2024-05-20 14:58:54 +0530

    ISNE-101-hrms

--- business-services/egov-hrms/src/main/java/org/egov/hrms/web/contract/User.java
+++ business-services/egov-hrms/src/main/java/org/egov/hrms/web/contract/User.java
@@ -2,10 +2,10 @@ package org.egov.hrms.web.contract;
 
 import java.util.List;
 
-import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 import org.egov.hrms.model.Role;
 import org.egov.hrms.model.enums.GuardianRelation;
