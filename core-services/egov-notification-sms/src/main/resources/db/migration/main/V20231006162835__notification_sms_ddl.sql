8a2d49d51930b101e92d47ad82f0f2537b59464d
pradeep-egov <pradeep@egovernments.org>
2024-07-25 14:41:49 +0530

    Update V20231006162835__notification_sms_ddl.sql

--- core-services/egov-notification-sms/src/main/resources/db/migration/main/V20231006162835__notification_sms_ddl.sql
+++ core-services/egov-notification-sms/src/main/resources/db/migration/main/V20231006162835__notification_sms_ddl.sql
@@ -7,11 +7,3 @@ CREATE TABLE IF NOT EXISTS  eg_notification_sms (
     createdtime bigint,
     tenant_id VARCHAR(50)
 );
-CREATE SEQUENCE seq_eg_notification_sms1
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
----ALTER TABLE eg_notification_sms ADD CONSTRAINT eg_notification_sms_pkey PRIMARY KEY (id);
-
