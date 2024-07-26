98bd7f9af4132a7a38c4f691a4e475602192911a
Shubham Gupta <75482143+shubhamdevgupta@users.noreply.github.com>
2024-07-26 13:05:46 +0530

    Update V20240726122840__alter_connection_v3.sql

--- municipal-services/ws-services/src/main/resources/db/migration/ddl/V20240726122840__alter_connection_v3.sql
+++ municipal-services/ws-services/src/main/resources/db/migration/ddl/V20240726122840__alter_connection_v3.sql
@@ -2,3 +2,4 @@ ALTER TABLE eg_ws_connection_audit ADD COLUMN IF NOT EXISTS ihlDetail varchar(25
 ALTER TABLE eg_ws_connection_audit ADD COLUMN IF NOT EXISTS sbmAccountno varchar(256);
 ALTER TABLE eg_ws_connection_audit ADD COLUMN IF NOT EXISTS schemeId varchar(256);
 ALTER TABLE eg_ws_connection_audit ADD COLUMN IF NOT EXISTS schemeName varchar(256);
+ALTER TABLE eg_ws_connection_audit ADD COLUMN IF NOT EXISTS accesIhl varchar(256);
