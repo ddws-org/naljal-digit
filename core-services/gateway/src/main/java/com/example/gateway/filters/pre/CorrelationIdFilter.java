26466ae99ed8213e6dd8a1e5970ba9c3bffd7673
palak-egov <palak.garg@egovernments.org>
2024-09-05 11:20:03 +0530

    [ISNE-244] adding missing code changes

--- core-services/gateway/src/main/java/com/example/gateway/filters/pre/CorrelationIdFilter.java
+++ core-services/gateway/src/main/java/com/example/gateway/filters/pre/CorrelationIdFilter.java
@@ -13,7 +13,6 @@ import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.gateway.filter.factory.rewrite.ModifyRequestBodyGatewayFilterFactory;
 import org.springframework.core.Ordered;
-import org.springframework.http.codec.multipart.Part;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
@@ -35,28 +34,21 @@ public class CorrelationIdFilter implements GlobalFilter, Ordered {
 
     private CorrIdFormDataFilterHelper correlationIdFormDataFilterHelper;
 
-    private ApplicationProperties applicationProperties;
-
     public CorrelationIdFilter(ModifyRequestBodyGatewayFilterFactory modifyRequestBodyGatewayFilter, CorrelationIdFilterHelper correlationIdFilterHelper,
-                               CorrIdFormDataFilterHelper correlationIdFormDataFilterHelper, ApplicationProperties applicationProperties) {
+                               CorrIdFormDataFilterHelper correlationIdFormDataFilterHelper) {
 
         this.modifyRequestBodyFilter = modifyRequestBodyGatewayFilter;
         this.correlationIdFilterHelper = correlationIdFilterHelper;
         this.correlationIdFormDataFilterHelper = correlationIdFormDataFilterHelper;
 
-        this.applicationProperties = applicationProperties;
     }
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
 
         String contentType = exchange.getRequest().getHeaders().getFirst(HttpHeaders.CONTENT_TYPE);
-        String endPointPath = exchange.getRequest().getPath().value();
 
-        if(endPointPath.contains("/filestore")){
-            return chain.filter(exchange);
-        }
-        else if (contentType != null && (contentType.contains("multipart/form-data") || contentType.contains("application/x-www-form-urlencoded"))) {
+        if (contentType != null && (contentType.contains("multipart/form-data") || contentType.contains("application/x-www-form-urlencoded"))) {
             return modifyRequestBodyFilter.apply(new ModifyRequestBodyGatewayFilterFactory.Config()
                     .setRewriteFunction(MultiValueMap.class, MultiValueMap.class, correlationIdFormDataFilterHelper))
                     .filter(exchange, chain);
