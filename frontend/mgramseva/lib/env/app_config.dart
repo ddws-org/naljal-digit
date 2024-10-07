d9b317918683c10562d383e49fda9e0872caa14d
Hari-egov <hariprasad@egovernments.org>
2024-10-07 23:16:39 +0530

    madhyapradesh build

--- frontend/mgramseva/lib/env/app_config.dart
+++ frontend/mgramseva/lib/env/app_config.dart
@@ -44,9 +44,9 @@ Future<Map<String, dynamic>> get devConstants async {
       Constants.STATE_CODE = dotenv.env['STATE_LEVEL_TENANT_ID']??'';
     }
     return {
-      _baseUrl: "https://naljalseva.jjm.gov.in/uat/" + state + (state.isNotEmpty?"/":''),
+      // _baseUrl: "https://naljalseva.jjm.gov.in/uat/" + state + (state.isNotEmpty?"/":''),
       // _baseUrl: "https://naljal-uat.digit.org/" + state + (state.isNotEmpty?"/":''),
-      // _baseUrl: window.location.origin + "/" + state + (state.isNotEmpty?"/":''),
+      _baseUrl: window.location.origin + "/" + state + (state.isNotEmpty?"/":''),
     };
 }
 Future<Map<String, dynamic>> get devConstantsMobile async {
@@ -60,7 +60,7 @@ Future<Map<String, dynamic>> get devConstantsMobile async {
       Constants.STATE_CODE = dotenv.env['STATE_LEVEL_TENANT_ID']??'';
     }
     return {
-      _baseUrl: "https://naljalseva.jjm.gov.in/uat/"+state+"/",
+      _baseUrl: "https://naljalseva.jjm.gov.in/uat/",
       // _baseUrl: "https://naljal-uat.digit.org/",
     };
 }
