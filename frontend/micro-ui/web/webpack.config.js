f8f84110fb696ce621f4e44962caf6cb21241da6
Hari-egov <hariprasad@egovernments.org>
2024-10-21 11:48:00 +0530

    config  changes

--- frontend/micro-ui/web/webpack.config.js
+++ frontend/micro-ui/web/webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "build"),
-    publicPath: `/mgramseva-web/`,
+    publicPath: `auto`,
   },
   optimization: {
     splitChunks: {
