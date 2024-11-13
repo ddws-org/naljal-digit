2b5a9409ff2b94959fd64b5646d0dc84d5a132b3
Hari-egov <168806306+Hari-egov@users.noreply.github.com>
2024-11-13 17:17:24 +0530

    Update Request.js

--- frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/atoms/Utils/Request.js
+++ frontend/micro-ui/web/micro-ui-internals/packages/libraries/src/services/atoms/Utils/Request.js
@@ -69,7 +69,8 @@ export const Request = async ({
   multipartData = {},
   reqTimestamp = false,
 }) => {
-  url = `/${window.globalConfigs.getConfig("STATE_PREFIX_CODE")}${url}`;
+  url = `/uat${url}`;
+  //url = `/${window.globalConfigs.getConfig("STATE_PREFIX_CODE")}${url}`;
   const ts = new Date().getTime();
   if (method.toUpperCase() === "POST") {
    
