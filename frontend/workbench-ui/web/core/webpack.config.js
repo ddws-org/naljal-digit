2905feb7d9df18f806b966b5c6c36ebd1b877ecb
Hari-eGov <hariprasad@egovernments.org>
2024-11-15 17:34:51 +0530

    babel

--- frontend/workbench-ui/web/core/webpack.config.js
+++ frontend/workbench-ui/web/core/webpack.config.js
@@ -12,7 +12,13 @@ module.exports = {
       {
         test: /\.(js)$/,
         exclude: /node_modules/,
-        use: ["babel-loader"],
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-object-rest-spread"]
+          }
+        }
       },
       {
         test: /\.css$/i,
