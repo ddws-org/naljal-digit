f51b60cb53ef3c1ebd6dd57a77b9caf0fa46cdc3
Hari-eGov <hariprasad@egovernments.org>
2024-12-18 11:48:26 +0530

    console.log("PUBLIC_PATH:1", process.env);

--- frontend/micro-ui/web/webpack.config.js
+++ frontend/micro-ui/web/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
+console.log("PUBLIC_PATH:1", process.env);
 module.exports = {
   // mode: 'development',
   entry:  './src/index.js',
